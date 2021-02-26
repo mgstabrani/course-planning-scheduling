86da22c95f9c69a7280afbedbca5871f49b24340
mgstabrani <mgstabrani19@gmail.com>
2021-02-26 21:19:45 +0700

    Add topologicalSort.py

---
+++ src/topologicalSort.py
@@ -0,0 +1,20 @@
+from graf import *
+
+def findZeroDegree(graf):
+    solution = []
+    for i in range(len(graf)):
+        if(degree(graf,graf[i][0]) == 0):
+            solution.append(i)
+    return solution
+
+def topologicalSort(graf):
+    solution = []
+    semester = 0
+    while(len(graf) > 0):
+        solution.append([])
+        zeroDegree = findZeroDegree(graf)
+        for i in range(len(zeroDegree)):
+            solution[semester].append(graf[zeroDegree[i]-i][0])
+            deleteNode(graf, graf[zeroDegree[i]-i][0])
+        semester += 1
+    return solution
